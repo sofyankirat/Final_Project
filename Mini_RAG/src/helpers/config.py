b731a206043da7172bc99c436cf7acf9ff2e70ea
Sofyan Kirat <www.sofyankirat123@gmail.com>
2026-06-26 17:30:33 +0200

    fix: increase vector index threshold to 10000

--- Mini_RAG/src/helpers/config.py
+++ Mini_RAG/src/helpers/config.py
@@ -38,7 +38,7 @@ class Settings(BaseSettings):
     VECTOR_DB_BACKEND : str
     VECTOR_DB_PATH : str
     VECTOR_DB_DISTANCE_METHOD: str = None
-    VECTOR_DB_PGVEC_INDEX_THRESHOLD: int = 100
+    VECTOR_DB_PGVEC_INDEX_THRESHOLD: int = 10000
 
     PRIMARY_LANG: str = "ar"
     DEFAULT_LANG: str = "en"
